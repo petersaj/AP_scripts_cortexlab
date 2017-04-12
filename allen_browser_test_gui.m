7a77b196597c0ae5863535fe776b3692b552eb07
Andrew Peters <peters.andrew.j@gmail.com>
2017-04-12 16:07:51 +0100

    update

--- allen_browser_test_gui.m
+++ allen_browser_test_gui.m
@@ -1,6 +1,9 @@
 function allen_browser_test_gui(tv,av,st,bregma)
 % allen_browser_test_gui(tv,av,st)
 
+% Initialize gui_data structure
+gui_data = struct;
+
 % If not already loaded in, load in atlas
 if nargin < 4
     cd('C:\Users\Andrew\OneDrive for Business\Documents\Atlases\AllenCCF')
@@ -21,6 +24,7 @@ set(gca, 'ZDir', 'reverse')
 axis equal
 axis off
 view([-30,25]);
+caxis([0 300]);
 
 % (coordinates in: plot [ap,ml,dv], volume [ap,dv,ml])
 [ap_max,dv_max,ml_max] = size(tv);
@@ -30,6 +34,7 @@ ylim([-10,ml_max+10])
 zlim([-10,dv_max+10])
 
 % Plot outline of the brain
+
 % (with downsampled polygons)
 % slice_spacing = 10;
 % target_volume = permute(av(1:slice_spacing:end,1:slice_spacing:end,1:slice_spacing:end) > 1,[3,1,2]);
@@ -39,9 +44,9 @@ zlim([-10,dv_max+10])
 % cortex_wire_h = patch('Vertices',structure_wire.vertices*slice_spacing, ...
 %     'Faces',structure_wire.faces, ...
 %     'FaceColor','none','EdgeColor',target_structure_color);
+
 % (with Nick's method)
 gridIn3D(double(av > 1),0.5,80,bregma);
-
 
 % Outline structures
 structure_alpha = 0.2;
@@ -81,12 +86,7 @@ probe_angle_dv = 45;
 rotate(probe_ref_line,[0,0,1],probe_angle_ml,probe_ref_top)
 rotate(probe_ref_line,[0,1,0],probe_angle_dv,probe_ref_top)
 
-% set up probe-angle plane
-slice_plot = update_slice(surface('EdgeColor','none'),tv,av,probe_ref_line);
-
 % Create and store guidata
-gui_data = struct;
-
 gui_data.tv = tv;
 gui_data.av = av;
 gui_data.st = st;
@@ -97,18 +97,17 @@ gui_data.bregma = bregma;
 gui_data.handles.structure_patch_h = structure_patch_h;
 
 gui_data.handles.axes_3d = axes_3d;
-gui_data.handles.slice_plot = slice_plot;
+gui_data.handles.slice_plot = surface('EdgeColor','none');
 gui_data.handles.probe_ref_line = probe_ref_line;
 
-
-
-% Set functions for mouse wheel / button press and pass necessary handles
+% Set functions for key presses
 set(probe_atlas_gui,'KeyPressFcn',@key_press); 
-
-
 
 % Upload gui_data
 guidata(probe_atlas_gui, gui_data);
+
+% Display the first slice
+update_slice(probe_atlas_gui);
 
 end
 
@@ -125,40 +124,36 @@ switch eventdata.Key
         ap_offset = -20;
         
         set(gui_data.handles.probe_ref_line,'XData',get(gui_data.handles.probe_ref_line,'XData') + ap_offset);
-        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
-            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        update_slice(probe_atlas_gui);
         
     case 'downarrow'
         
         ap_offset = 20;
         
         set(gui_data.handles.probe_ref_line,'XData',get(gui_data.handles.probe_ref_line,'XData') + ap_offset);
-        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
-            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        update_slice(probe_atlas_gui);
         
     case 'rightarrow'
         
         ml_offset = 20;
         
         set(gui_data.handles.probe_ref_line,'YData',get(gui_data.handles.probe_ref_line,'YData') + ml_offset);
-        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
-            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        update_slice(probe_atlas_gui);
         
     case 'leftarrow'
         
         ml_offset = -20;
         
         set(gui_data.handles.probe_ref_line,'YData',get(gui_data.handles.probe_ref_line,'YData') + ml_offset);
-        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
-            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        update_slice(probe_atlas_gui);
         
     case 'm'
         % Toggle face visibility of target structure
         curr_structure_FaceAlpha = [gui_data.handles.structure_patch_h.FaceAlpha];
         if any(curr_structure_FaceAlpha)
-            new_structure_FaceAlpha = 0.2;
-        else
             new_structure_FaceAlpha = 0;
+        else
+            new_structure_FaceAlpha = 0.2;
         end
         [gui_data.handles.structure_patch_h.FaceAlpha] = ...
             deal(new_structure_FaceAlpha);
@@ -174,18 +169,23 @@ switch eventdata.Key
         
     case 'u'
         % Update slice
-        gui_data.handles.slice_plot = update_slice( ...
-            gui_data.handles.slice_plot,gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        update_slice(probe_atlas_gui);
         
     case 'r'
         % Toggle 3D rotation
-        rotate3d(probe_atlas_gui)
-        %(this is annoyingly complicated: need to restore key-press
-        %functionality after turning on 3D rotation)       
-        hManager = uigetmodemanager(probe_atlas_gui);
-        [hManager.WindowListenerHandles.Enabled] = deal(false);
-        set(probe_atlas_gui,'KeyPressFcn',@key_press); 
-        
+        h = rotate3d(gui_data.handles.axes_3d);
+        switch h.Enable
+            case 'off'
+                h.Enable = 'on';
+                % Update the slice whenever a rotation is completed  
+                h.ActionPostCallback = @update_slice;
+                %(need to restore key-press functionality with rotation)
+                hManager = uigetmodemanager(probe_atlas_gui);
+                [hManager.WindowListenerHandles.Enabled] = deal(false);
+                set(probe_atlas_gui,'KeyPressFcn',@key_press);                                           
+            case 'on'
+                h.Enable = 'off';
+        end
 end
 
 % Upload gui_data
@@ -193,14 +193,17 @@ guidata(probe_atlas_gui, gui_data);
 
 end
 
-function slice_plot = update_slice(slice_plot,tv,av,probe_ref_line)
+function update_slice(probe_atlas_gui,varargin)
+
+% Get guidata
+gui_data = guidata(probe_atlas_gui);
 
 % Get current position of camera
 curr_campos = campos;
 
 % Get probe vector
-probe_ref_top = [probe_ref_line.XData(1),probe_ref_line.YData(1),probe_ref_line.ZData(1)];
-probe_ref_bottom = [probe_ref_line.XData(2),probe_ref_line.YData(2),probe_ref_line.ZData(2)];
+probe_ref_top = [gui_data.handles.probe_ref_line.XData(1),gui_data.handles.probe_ref_line.YData(1),gui_data.handles.probe_ref_line.ZData(1)];
+probe_ref_bottom = [gui_data.handles.probe_ref_line.XData(2),gui_data.handles.probe_ref_line.YData(2),gui_data.handles.probe_ref_line.ZData(2)];
 probe_vector = probe_ref_top - probe_ref_bottom;
 
 % Get probe-camera vector
@@ -216,7 +219,8 @@ normal_vector = cross(plot_vector,probe_vector);
 plane_offset = -(normal_vector*probe_ref_top');
 
 % Define a plane within the boundaries orthogonal to viewing axis on probe
-[plane_x,plane_y] = meshgrid(1:5:size(tv,1),1:5:size(tv,3));
+slice_px_space = 3;
+[plane_x,plane_y] = meshgrid(1:slice_px_space:size(gui_data.tv,1),1:slice_px_space:size(gui_data.tv,3));
 
 plane_z = ...
     (normal_vector(1)*plane_x+normal_vector(2)*plane_y + plane_offset)/ ...
@@ -228,27 +232,29 @@ y_idx = plane_y;
 z_idx = round(plane_z);
 
 % Find plane coordinates in bounds with the volume
-use_zd = z_idx > 0 & z_idx < size(tv,2);
-curr_slice_idx = sub2ind(size(tv),x_idx(use_zd),z_idx(use_zd),y_idx(use_zd));
+use_zd = z_idx > 0 & z_idx < size(gui_data.tv,2);
+curr_slice_idx = sub2ind(size(gui_data.tv),x_idx(use_zd),z_idx(use_zd),y_idx(use_zd));
 
 %(coordinates in: plot [ap,ml,dv], volume [ap,dv,ml])
 
 % Find plane coordinates that contain brain
 curr_slice_isbrain = false(size(use_zd));
-curr_slice_isbrain(use_zd) = av(curr_slice_idx) > 1;
+curr_slice_isbrain(use_zd) = gui_data.av(curr_slice_idx) > 1;
 
 % Index coordinates in bounds + with brain
-grab_pix_idx = sub2ind(size(tv),x_idx(curr_slice_isbrain),z_idx(curr_slice_isbrain),y_idx(curr_slice_isbrain));
+grab_pix_idx = sub2ind(size(gui_data.tv),x_idx(curr_slice_isbrain),z_idx(curr_slice_isbrain),y_idx(curr_slice_isbrain));
 
 % Grab pixels from volume
 curr_slice = nan(size(use_zd));
-curr_slice(curr_slice_isbrain) = tv(grab_pix_idx);
+curr_slice(curr_slice_isbrain) = gui_data.tv(grab_pix_idx);
 
 % Update the slice display
-set(slice_plot,'XData',plane_x,'YData',plane_y,'ZData',plane_z,'CData',curr_slice);
+set(gui_data.handles.slice_plot,'XData',plane_x,'YData',plane_y,'ZData',plane_z,'CData',curr_slice);
+
+% Upload gui_data
+guidata(probe_atlas_gui, gui_data);
 
 end
-
 
 
 
