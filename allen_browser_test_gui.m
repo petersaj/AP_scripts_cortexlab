427b28e716bd00191b756cef058ed8f9f4db72a5
Andrew Peters <peters.andrew.j@gmail.com>
2017-04-12 15:04:22 +0100

    update

--- allen_browser_test_gui.m
+++ allen_browser_test_gui.m
@@ -3,7 +3,6 @@ function allen_browser_test_gui(tv,av,st,bregma)
 
 % If not already loaded in, load in atlas
 if nargin < 4
-    % Loading Allen CCF atlas
     cd('C:\Users\Andrew\OneDrive for Business\Documents\Atlases\AllenCCF')
     tv = readNPY('template_volume_10um.npy'); % grey-scale "background signal intensity"
     av = readNPY('annotation_volume_10um_by_index.npy'); % the number at each pixel labels the area, see note below
@@ -12,7 +11,7 @@ if nargin < 4
 end
 
 % Set up the gui and axes
-probe_atlas_gui = figure;
+probe_atlas_gui = figure('Toolbar','none','color','w');
 colormap(gray);
 
 axes_3d = axes;
@@ -23,7 +22,7 @@ axis equal
 axis off
 view([-30,25]);
 
-% coordinates in plot: [ap,ml,dv]
+% (coordinates in: plot [ap,ml,dv], volume [ap,dv,ml])
 [ap_max,dv_max,ml_max] = size(tv);
 axis manual
 xlim([-10,ap_max+10])
@@ -44,7 +43,10 @@ zlim([-10,dv_max+10])
 gridIn3D(double(av > 1),0.5,80,bregma);
 
 
-% Outline the CP
+% Outline structures
+structure_alpha = 0.2;
+structure_patch_h = gobjects;
+
 target_structure = 'CP';
 st_idx = find(strcmp(st.acronym,target_structure));
 target_structure_color = hex2dec(reshape(st.color_hex_triplet{st_idx},3,[]))./255;
@@ -52,9 +54,20 @@ slice_spacing = 10;
 structure_patch = isosurface(permute(av(1:slice_spacing:end,1:slice_spacing:end,1:slice_spacing:end) == st_idx,[3,1,2]),0);
 structure_wire = reducepatch(structure_patch.faces,structure_patch.vertices,0.01);
 
-structure_patch_h = patch('Vertices',structure_patch.vertices*slice_spacing, ...
+structure_patch_h(1) = patch('Vertices',structure_patch.vertices*slice_spacing, ...
     'Faces',structure_patch.faces, ...
-    'FaceColor',target_structure_color,'EdgeColor','none','FaceAlpha',0.5);
+    'FaceColor',target_structure_color,'EdgeColor','none','FaceAlpha',structure_alpha);
+
+target_structure = 'SNr';
+st_idx = find(strcmp(st.acronym,target_structure));
+target_structure_color = hex2dec(reshape(st.color_hex_triplet{st_idx},3,[]))./255;
+slice_spacing = 10;
+structure_patch = isosurface(permute(av(1:slice_spacing:end,1:slice_spacing:end,1:slice_spacing:end) == st_idx,[3,1,2]),0);
+structure_wire = reducepatch(structure_patch.faces,structure_patch.vertices,0.01);
+
+structure_patch_h(2) = patch('Vertices',structure_patch.vertices*slice_spacing, ...
+    'Faces',structure_patch.faces, ...
+    'FaceColor',target_structure_color,'EdgeColor','none','FaceAlpha',structure_alpha);
 
 % Set up the probe
 probe_ref_top = [ap_max,bregma(3),0];
@@ -69,7 +82,7 @@ rotate(probe_ref_line,[0,0,1],probe_angle_ml,probe_ref_top)
 rotate(probe_ref_line,[0,1,0],probe_angle_dv,probe_ref_top)
 
 % set up probe-angle plane
-slice_plot = update_slice(tv,av,probe_ref_line);
+slice_plot = update_slice(surface('EdgeColor','none'),tv,av,probe_ref_line);
 
 % Create and store guidata
 gui_data = struct;
@@ -112,44 +125,57 @@ switch eventdata.Key
         ap_offset = -20;
         
         set(gui_data.handles.probe_ref_line,'XData',get(gui_data.handles.probe_ref_line,'XData') + ap_offset);
-        set(gui_data.handles.slice_plot,'Visible','off')
+        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
+            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
         
     case 'downarrow'
         
         ap_offset = 20;
         
         set(gui_data.handles.probe_ref_line,'XData',get(gui_data.handles.probe_ref_line,'XData') + ap_offset);
-        set(gui_data.handles.slice_plot,'Visible','off')
+        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
+            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
         
     case 'rightarrow'
         
         ml_offset = 20;
         
         set(gui_data.handles.probe_ref_line,'YData',get(gui_data.handles.probe_ref_line,'YData') + ml_offset);
-        set(gui_data.handles.slice_plot,'Visible','off')
+        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
+            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
         
     case 'leftarrow'
         
         ml_offset = -20;
         
         set(gui_data.handles.probe_ref_line,'YData',get(gui_data.handles.probe_ref_line,'YData') + ml_offset);
-        set(gui_data.handles.slice_plot,'Visible','off')
+        gui_data.handles.slice_plot = update_slice(gui_data.handles.slice_plot, ...
+            gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
         
     case 'm'
         % Toggle face visibility of target structure
-        curr_structure_FaceAlpha = gui_data.handles.structure_patch_h.FaceAlpha;
-        if ~curr_structure_FaceAlpha
-            new_structure_FaceAlpha = 0.5;
+        curr_structure_FaceAlpha = [gui_data.handles.structure_patch_h.FaceAlpha];
+        if any(curr_structure_FaceAlpha)
+            new_structure_FaceAlpha = 0.2;
         else
             new_structure_FaceAlpha = 0;
         end
-        gui_data.handles.structure_patch_h.FaceAlpha = ...
-            new_structure_FaceAlpha;
+        [gui_data.handles.structure_patch_h.FaceAlpha] = ...
+            deal(new_structure_FaceAlpha);
+        
+    case 's'
+        % Toggle slice visibility
+        switch gui_data.handles.slice_plot.Visible
+            case 'on'
+                gui_data.handles.slice_plot.Visible = 'off';
+            case 'off'
+                gui_data.handles.slice_plot.Visible = 'on';
+        end
         
     case 'u'
         % Update slice
-        delete(gui_data.handles.slice_plot);
-        gui_data.handles.slice_plot = update_slice(gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
+        gui_data.handles.slice_plot = update_slice( ...
+            gui_data.handles.slice_plot,gui_data.tv,gui_data.av,gui_data.handles.probe_ref_line);
         
     case 'r'
         % Toggle 3D rotation
@@ -167,7 +193,7 @@ guidata(probe_atlas_gui, gui_data);
 
 end
 
-function slice_plot = update_slice(tv,av,probe_ref_line)
+function slice_plot = update_slice(slice_plot,tv,av,probe_ref_line)
 
 % Get current position of camera
 curr_campos = campos;
@@ -190,40 +216,36 @@ normal_vector = cross(plot_vector,probe_vector);
 plane_offset = -(normal_vector*probe_ref_top');
 
 % Define a plane within the boundaries orthogonal to viewing axis on probe
-[plane_x,plane_y] = meshgrid(1:size(tv,1),1:size(tv,3));
+[plane_x,plane_y] = meshgrid(1:5:size(tv,1),1:5:size(tv,3));
 
 plane_z = ...
     (normal_vector(1)*plane_x+normal_vector(2)*plane_y + plane_offset)/ ...
     -normal_vector(3);
 
-% Plot the plane
-slice_plot = surf(plane_x,plane_y,plane_z,'EdgeColor','none');
-
 % Get the coordiates on the plane
-xd = round(slice_plot.XData);
-yd = round(slice_plot.YData);
-zd = round(slice_plot.ZData);
+x_idx = plane_x;
+y_idx = plane_y;
+z_idx = round(plane_z);
 
 % Find plane coordinates in bounds with the volume
-use_xd = xd > 0 & xd < size(tv,1);
-use_yd = yd > 0 & yd < size(tv,3);
-use_zd = zd > 0 & zd < size(tv,2);
-use_slice_px = use_xd & use_yd & use_zd;
-curr_slice_idx = sub2ind(size(tv),xd(use_slice_px),zd(use_slice_px),yd(use_slice_px));
+use_zd = z_idx > 0 & z_idx < size(tv,2);
+curr_slice_idx = sub2ind(size(tv),x_idx(use_zd),z_idx(use_zd),y_idx(use_zd));
+
+%(coordinates in: plot [ap,ml,dv], volume [ap,dv,ml])
 
 % Find plane coordinates that contain brain
-curr_slice_isbrain = false(size(use_slice_px));
-curr_slice_isbrain(use_slice_px) = av(curr_slice_idx) > 1;
+curr_slice_isbrain = false(size(use_zd));
+curr_slice_isbrain(use_zd) = av(curr_slice_idx) > 1;
 
 % Index coordinates in bounds + with brain
-grab_pix_idx = sub2ind(size(tv),xd(curr_slice_isbrain),zd(curr_slice_isbrain),yd(curr_slice_isbrain));
+grab_pix_idx = sub2ind(size(tv),x_idx(curr_slice_isbrain),z_idx(curr_slice_isbrain),y_idx(curr_slice_isbrain));
 
 % Grab pixels from volume
-curr_slice = nan(size(use_slice_px));
+curr_slice = nan(size(use_zd));
 curr_slice(curr_slice_isbrain) = tv(grab_pix_idx);
 
-% Display slice on plane
-set(slice_plot,'CData',curr_slice);
+% Update the slice display
+set(slice_plot,'XData',plane_x,'YData',plane_y,'ZData',plane_z,'CData',curr_slice);
 
 end
 
