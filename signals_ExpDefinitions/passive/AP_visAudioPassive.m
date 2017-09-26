8d96c34bc377e948ac87924c6c0d50447be1ef14
Andrew Peters <peters.andrew.j@gmail.com>
2017-09-26 12:21:16 +0100

    update

--- signals_ExpDefinitions/passive/AP_visAudioPassive.m
+++ signals_ExpDefinitions/passive/AP_visAudioPassive.m
@@ -1,153 +1,86 @@
 function AP_visAudioPassive(t, events, parameters, visStim, inputs, outputs, audio)
 % AP 2017-09-22: present random tones and visual stimuli
 
-
-
 %% Set up stimuli
 
 stimTime = 0.5;
 itiTimeRange= [1:0.1:2];
 
 % Sound stim
-%audioFreqRange = linspace(5000,20000,5);
-audioFreqRange = linspace(440,5000,3);
+audioFreqRange = linspace(5000,20000,5);
 audioSampleRate = 192e3;
 
 audioRampDuration = 0.01;
 audioChannels = 2;
 
-% 
-% toneSamples = onsetToneAmplitude*events.expStart.map(@(x) ...
-%     aud.pureTone(onsetToneFreq,stimTime,audioSampleRate, ...
-%     rampDuration,audioChannels));
-% 
-% 
-% 
-% 
-% 
-% audio_t = [0:1/audioSampleRate:stimTime];
-% audio_freq = linspace(audio_freq_range(1),audio_freq_range(2),length(audio_t));
-% 
-% rampSamples = round(audioSampleRate*rampDuration);
-% audio_vol = [linspace(0,1,rampSamples),ones(1,length(audio_t)-2*rampSamples),linspace(1,0,rampSamples)];
-% 
-% audioSweepUp = sin(audio_t.*audio_freq.*2*pi).*audio_vol;
-% audioSweepDown = fliplr(audioSweepUp);
-% 
-% audioStim = [audioSweepUp;audioSweepDown];
-% 
-% % Visual stim
-% 
-% vis_azimuths = [-60,0,60];
-% spatialFrequency = 0.01;
-% contrast = 1;
-% vis_azimuth_sigma = [45,360];
-% vis_elevation_sigma = [360,45];
-% stimFlickerFrequency = 5;
-% orientation = 45;
+% Visual stim
+vis_az_el_sig = {[-60,90,45,360],[0,90,45,360],[60,90,45,360]};
+spatialFrequency = 0.01;
+contrast = 1;
+stimFlickerFrequency = 5;
+orientation = 45; 
 
 
 %% Stim times
 
-n_audio_reps = 2;
-n_visual_reps = 50;
+n_audio_reps = 5;
+n_visual_reps = 5;
 
-audio_freq_uniform = repmat(audioFreqRange,n_audio_reps,1);
-audio_freq_shuffle = audio_freq_uniform(randperm(numel(audio_freq_uniform)));
+% Audio
+audio_stim_uniform = repmat(audioFreqRange,n_audio_reps,1);
+audio_stim_shuffle = audio_stim_uniform(randperm(numel(audio_stim_uniform)));
 
-
-audio_itiTimes = randsample(itiTimeRange,n_audio_reps,true);
+audio_itiTimes = randsample(itiTimeRange,length(audio_stim_shuffle),true);
 audio_startTimes = cumsum(audio_itiTimes + stimTime);
 
+% Visual
+visual_stim_uniform = repmat(vis_az_el_sig,n_visual_reps,1);
+visual_stim_shuffle = visual_stim_uniform(randperm(numel(visual_stim_uniform)));
 
-
-
-% visual_itiTimes = randsample(itiTimeRange,n_visual_reps,true);
-% visual_startTimes = cumsum(visual_itiTimes + stimTime);
-% visual_stopTimes = visual_startTimes + stimTime;
+visual_itiTimes = randsample(itiTimeRange,length(visual_stim_shuffle),true);
+visual_startTimes = cumsum(visual_itiTimes + stimTime);
 
 
 %% Present stim
 
-
-%audioOnset = t.map(@(x) find(x > audio_startTimes,1,'last'));
-%audioOnset = t.scan(@advanceStim,audio_startTimes);
-%audioOnset = t.ge(audio_startTimes);
+% Audio 
 audioOnset = t.map(@(t) sum(t > audio_startTimes)).skipRepeats;
 
-
-toneSamples = audioOnset.map(@(x) ...
-    aud.pureTone(audio_freq_shuffle(x),stimTime,audioSampleRate, ...
+toneSamples = audioOnset.at(audioOnset).map(@(x) ...
+    aud.pureTone(audio_stim_shuffle(x),stimTime,audioSampleRate, ...
     audioRampDuration,audioChannels));
 audio.playTone = toneSamples;
 
-% (TRYING TO INDEX FOR FREQUENCY BUT NOT WORKING AT THE MOMENT)
+% Visual
+visualOnset = t.map(@(t) sum(t > visual_startTimes)).skipRepeats;
+
+stimFlicker = mod(skipRepeats(floor((t - t.at(visualOnset))/(1/stimFlickerFrequency))),2);
+stim = vis.grating(t, 'square', 'gaussian');
+stim.spatialFrequency = spatialFrequency;
+stim.contrast = contrast;
+stim.orientation = orientation;
+stim.phase = pi*stimFlicker;
+
+stim.azimuth = visualOnset.at(visualOnset).map(@(x) visual_stim_shuffle{x}(1));
+stim.elevation = visualOnset.at(visualOnset).map(@(x) visual_stim_shuffle{x}(2));
+stim.sigma = visualOnset.at(visualOnset).map(@(x) visual_stim_shuffle{x}(3:4));
+stim.show = visualOnset.to(visualOnset.delay(stimTime));
+visStim.stim = stim;
 
 
-% 
-% beepFreq = beepOnset.scan(@(x,y)rand()*(maxFreq-minFreq)+minFreq,0);
-% 
-% toneSamples = beepAmplitude*...
-%   mapn(beepFreq, beepDur, audioSR, 0.02, @aud.pureTone);
-
-
-endTrial = t.ge(audio_startTimes(end)).skipRepeats;
+endTrial = t.ge(max(audio_startTimes(end),visual_startTimes(end))).skipRepeats;
 
 %% Events
 
-events.t = t;
 events.audioOnset = audioOnset;
+events.audioParams = events.expStart.map(@(x) audio_stim_shuffle);
+
+events.visualOnset = visualOnset;
+events.visualParams = events.expStart.map(@(x) visual_stim_shuffle);
+
 events.endTrial = endTrial;
 
-
-
-% %% Set up trial parameters and events
-% 
-% % Choose random stimulus on each trial
-% trialAzimuth = events.newTrial.mapn(@(x) randsample(stimAzimuths,1));
-% 
-% % Turn stim on for fixed interval
-% stimOn = at(true,events.newTrial); 
-% stimOff = stimOn.delay(stimTime);
-% 
-% % Start the ITI after the stimulus turns off
-% trialITI = events.newTrial.mapn(@(x) randsample(itiTimes,1));
-% endTrial = stimOff.delay(trialITI);
-% 
-% %% Visual stimuli
-% 
-% stimFlicker = mod(skipRepeats(floor((t - t.at(stimOn))/(1/stimFlickerFrequency))),2);
-% %stim.contrast = trialContrast.at(stimOn)*stimFlicker;
-% 
-% stim = vis.grating(t, 'square', 'gaussian');
-% stim.sigma = sigma;
-% stim.spatialFrequency = spatialFrequency;
-% stim.phase = pi*stimFlicker;
-% stim.azimuth = trialAzimuth.at(stimOn);
-% stim.contrast = contrast;
-% stim.orientation = orientation;
-% stim.show = stimOn.to(stimOff);
-% 
-% visStim.stim = stim;
-% 
-% %% Events
-% 
-% events.stimOn = stimOn;
-% events.stimOff = stimOff;
-% events.stimFlicker = stimFlicker;
-% events.trialAzimuth = trialAzimuth;
-% 
-% events.endTrial = endTrial;
-
-
-
 end
-
-function stimNum = advanceStim(stimOrder,t)
-stimNum = find(t > stimOrder,1,'last');
-end
-
-
 
 
 
