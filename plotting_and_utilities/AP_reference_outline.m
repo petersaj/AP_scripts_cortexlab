5adc1f275691111d5d93dde1cc50c7314d2a2be8
Andrew Peters <peters.andrew.j@gmail.com>
2017-12-12 18:37:32 +0000

    update

--- plotting_and_utilities/AP_reference_outline.m
+++ plotting_and_utilities/AP_reference_outline.m
@@ -3,6 +3,11 @@ function h = AP_reference_outline(reference_im)
 %
 % If a reference image is provided, bregma is defined there
 
+%% Define bregma
+
+% Hard-code microns per pixel
+um2pixel = 20.6;
+
 if exist('reference_im','var') && ~isempty(reference_im)
     temp_f = figure;
     imagesc(reference_im);
@@ -16,9 +21,10 @@ end
 
 hold on;
 
-pixel2um = 20.6;
+%% Plot mm grid
+
 spacing_um = 1000;
-spacing_pixels = spacing_um/pixel2um;
+spacing_pixels = spacing_um/um2pixel;
 
 xlines_pos = bregma_y + spacing_pixels*(ceil((min(ylim)-bregma_y)./spacing_pixels):floor((max(ylim)-bregma_y)./spacing_pixels));
 ylines_pos = bregma_x + spacing_pixels*(ceil((min(xlim)-bregma_x)./spacing_pixels):floor((max(xlim)-bregma_x)./spacing_pixels));
@@ -36,41 +42,46 @@ end
 h.bregma = plot(bregma_x,bregma_y,'.r','MarkerSize',30);
 
 
+%% Plot allen CCF outlines
+    
+% TO MAKE FROM SCRATCH
+% % Get first brain pixel from top-down, get annotation at that point
+% [~,top_down_depth] = max(av>1, [], 2);
+% top_down_depth = squeeze(top_down_depth);
+% 
+% [xx,yy] = meshgrid(1:size(top_down_depth,2), 1:size(top_down_depth,1));
+% top_down_annotation = reshape(av(sub2ind(size(av),yy(:),top_down_depth(:),xx(:))), size(av,1), size(av,3));
+% 
+% % Get all labelled areas
+% used_areas = unique(top_down_annotation(:));
+% 
+% % Restrict to only cortical areas
+% structure_id_path = cellfun(@(x) textscan(x(2:end),'%d', 'delimiter',{'/'}),st.structure_id_path);
+% 
+% ctx_path = [997,8,567,688,695,315];
+% ctx_idx = find(cellfun(@(id) length(id) > length(ctx_path) & ...
+%     all(id(min(length(id),length(ctx_path))) == ctx_path(min(length(id),length(ctx_path)))),structure_id_path));
+% 
+% plot_areas = intersect(used_areas,ctx_idx);
+% 
+% bregma = allenCCFbregma;
+% 
+% % Get outlines of all areas
+% top_down_cortical_area_boundaries = cell(size(plot_areas));
+% for curr_area_idx = 1:length(plot_areas)
+%     top_down_cortical_area_boundaries{curr_area_idx} = bwboundaries(top_down_annotation == plot_areas(curr_area_idx));
+% end
 
-%% for later? 
-
-if false
-    
-    % (from Nick)
-    % Get first brain pixel from top-down, get annotation at that point
-    [~,top_down_depth] = max(av>1, [], 2);
-    top_down_depth = squeeze(top_down_depth);
-        
-    [xx,yy] = meshgrid(1:size(top_down_depth,2), 1:size(top_down_depth,1));    
-    top_down_annotation = reshape(av(sub2ind(size(av),yy(:),top_down_depth(:),xx(:))), size(av,1), size(av,3));
-    
-    % Get all labelled areas
-    used_areas = unique(top_down_annotation(:));
-
-    % Restrict to only cortical areas
-    structure_id_path = cellfun(@(x) textscan(x(2:end),'%d', 'delimiter',{'/'}),st.structure_id_path);
-    
-    ctx_path = [997,8,567,688,695,315];
-    ctx_idx = find(cellfun(@(id) length(id) > length(ctx_path) & ...
-        all(id(min(length(id),length(ctx_path))) == ctx_path(min(length(id),length(ctx_path)))),structure_id_path));
-    
-    plot_areas = intersect(used_areas,ctx_idx);
-    
-    bregma = allenCCFbregma;
-    
-    figure; hold on;
-    for curr_area = plot_areas'       
-        curr_boundaries = bwboundaries(top_down_annotation == curr_area);
-        cellfun(@(x) plot((x(:,2)-bregma(3))/100,(x(:,1)-bregma(1))/100,'k'),curr_boundaries,'uni',false);
-    end
-  
-    
+% TO LOAD PREVIOUSLY GENERATED
+load('C:\Users\Andrew\OneDrive for Business\Documents\Atlases\AllenCCF\top_down_cortical_area_boundaries');
+bregma = allenCCFbregma;
+for curr_area_idx =1:length(top_down_cortical_area_boundaries)
+    cellfun(@(outline) plot(bregma_x + ((outline(:,2)-bregma(3))*10)/um2pixel, ...
+        bregma_y + ((outline(:,1)-bregma(1))*10)/um2pixel,'k'),top_down_cortical_area_boundaries{curr_area_idx},'uni',false);
 end
+
+
+
 
 
 
