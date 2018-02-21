f97bc5b823cecefa4dd20c48949b358f5133d752
Andrew Peters <peters.andrew.j@gmail.com>
2018-02-21 14:04:31 +0000

    update

---
+++ general/AP_reflect_widefield.m
@@ -0,0 +1,46 @@
+function im_reflected = AP_reflect_widefield(im)
+% Reflect image about the midline
+% (currently only uses master alignment)
+% this doesn't work perfectly so something's off - I've spent way too much
+% time on it and it's too close to be worth not just running with at the
+% moment
+
+warning('REFLECTION ISN''T PEFRECT');
+
+% Load bregma and master CCF tform
+bregma = allenCCFbregma;
+bregma(3) = bregma(3) + 0.5;
+ccf_tform_fn = ['C:\Users\Andrew\OneDrive for Business\Documents\Atlases\AllenCCF\ccf_tform'];
+load(ccf_tform_fn);
+
+midline_align = [1,0,0]*ccf_tform.T;
+midline_vector = [midline_align([1,2])./norm(midline_align([1,2])),0];
+
+um2pixel = 20.6;
+bregma_resize = bregma*(10/um2pixel);
+bregma_align = [bregma_resize([3,1]),1]*ccf_tform.T;
+
+% Translate bregma to origin, then reflect, then translate back
+I = eye(3);
+
+reflection_tform = I - 2*(midline_vector'*midline_vector);
+
+origin_translation_tform = I;
+origin_translation_tform(3,1:2) = -bregma_align(1:2);
+
+bregma_translation_tform = I;
+bregma_translation_tform(3,1:2) = bregma_align(1:2);
+
+tform_matrix = origin_translation_tform*reflection_tform*bregma_translation_tform;
+
+tform = affine2d;
+tform.T = tform_matrix;
+im_reflected = imwarp(im,tform,'Outputview',imref2d(size(im)));
+
+
+
+
+
+
+
+
