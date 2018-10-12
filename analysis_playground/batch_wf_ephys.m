566abb86aa9db75bcccd10b0fbcc8fcdfff24b2d
Andrew Peters <peters.andrew.j@gmail.com>
2018-10-12 17:48:12 +0100

    update
    update

--- analysis_playground/batch_wf_ephys.m
+++ analysis_playground/batch_wf_ephys.m
@@ -6547,6 +6547,121 @@ save_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\
 save_fn = ['all_trial_activity_Udf_kernel-str_' num2str(n_aligned_depths) '_depths_long'];
 save([save_path filesep save_fn],'-v7.3');
 
+%% Batch load and save activity from all choiceworld (common U - LONG, WF ONLY DAYS)
+
+n_aligned_depths = 4;
+
+animals = {'AP024','AP025','AP026','AP027','AP028','AP029'};
+
+fluor_all = cell(length(animals),1);
+mua_all = cell(length(animals),1);
+wheel_all = cell(length(animals),1);
+reward_all = cell(length(animals),1);
+D_all = cell(length(animals),1);
+
+for curr_animal = 1:length(animals)
+    
+    animal = animals{curr_animal};
+    protocol = 'vanillaChoiceworld';
+    experiments = AP_find_experiments(animal,protocol);
+    
+    experiments = experiments([experiments.imaging] & ~[experiments.ephys]);
+    
+    load_parts.cam = false;
+    load_parts.imaging = true;
+    load_parts.ephys = false;
+    
+    fluor_all{curr_animal} = cell(length(experiments),1);
+    mua_all{curr_animal} = cell(length(experiments),1);
+    wheel_all{curr_animal} = cell(length(experiments),1);
+    D_all{curr_animal} = cell(length(experiments),1);
+    
+    disp(['Loading ' animal]);
+    
+    for curr_day = 1:length(experiments)
+        
+        day = experiments(curr_day).day;
+        experiment = experiments(curr_day).experiment;
+        
+        % Load experiment
+        str_align = 'kernel';
+        AP_load_experiment;
+        
+        % Prepare fluorescence
+        % Convert U to master U
+        load('C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\wf_processing\wf_alignment\U_master.mat');
+        Udf_aligned = single(AP_align_widefield(animal,day,Udf));
+        fVdf_recast = ChangeU(Udf_aligned,fVdf,U_master);
+        % Set components to use
+        use_components = 1:200;
+        
+        % Get event-aligned activity
+        raster_window = [-0.5,3];
+        upsample_factor = 3;
+        raster_sample_rate = 1/(framerate*upsample_factor);
+        t = raster_window(1):raster_sample_rate:raster_window(2);
+        
+        % Align (only to stim)
+        use_align = stimOn_times;
+        use_align(isnan(use_align)) = 0;
+        
+        t_peri_event = bsxfun(@plus,use_align,t);
+        
+        % Fluorescence
+        event_aligned_V = ...
+            interp1(frame_t,fVdf_recast(use_components,:)',t_peri_event);
+        
+        % Wheel
+        event_aligned_wheel_raw = interp1(Timeline.rawDAQTimestamps, ...
+            wheel_position,t_peri_event);
+        event_aligned_wheel = bsxfun(@minus,event_aligned_wheel_raw, ...
+            nanmedian(event_aligned_wheel_raw(:,t < 0),2));
+        
+        % Reward
+        t_bins = [t_peri_event-raster_sample_rate/2,t_peri_event(:,end)+raster_sample_rate/2];      
+        event_aligned_reward = (cell2mat(arrayfun(@(x) ...
+            histcounts(reward_t_timeline,t_bins(x,:)), ...
+            [1:size(t_peri_event,1)]','uni',false))./raster_sample_rate) > 0;
+        
+        % Pick trials to use
+        use_trials = ...
+            trial_outcome ~= 0 & ...
+            ~signals_events.repeatTrialValues(1:n_trials) & ...
+            stim_to_feedback < 1.5;
+        
+        % Get behavioural data
+        D = struct;
+        D.stimulus = zeros(sum(use_trials),2);
+        
+        L_trials = signals_events.trialSideValues(1:n_trials) == -1;
+        R_trials = signals_events.trialSideValues(1:n_trials) == 1;
+        
+        D.stimulus(L_trials(use_trials),1) = signals_events.trialContrastValues(L_trials & use_trials);
+        D.stimulus(R_trials(use_trials),2) = signals_events.trialContrastValues(R_trials & use_trials);
+        
+        % D.response = (trial_choice(use_trials)'+1)/2+1;
+        D.response = 3-(abs((trial_choice(use_trials)'+1)/2)+1);
+        D.repeatNum = ones(sum(use_trials),1);
+        
+        % Store activity and stim
+        fluor_all{curr_animal}{curr_day} = event_aligned_V(use_trials,:,:,:);
+        wheel_all{curr_animal}{curr_day} = event_aligned_wheel(use_trials,:,:);
+        reward_all{curr_animal}{curr_day} = event_aligned_reward(use_trials,:,:);
+        D_all{curr_animal}{curr_day} = D;
+        
+        AP_print_progress_fraction(curr_day,length(experiments));
+    end
+    
+    clearvars -except n_aligned_depths animals curr_animal fluor_all mua_all wheel_all reward_all D_all
+    
+end
+clearvars -except n_aligned_depths fluor_all mua_all wheel_all reward_all D_all
+disp('Finished loading all')
+
+save_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\choiceworld';
+save_fn = ['all_trial_activity_Udf_kernel-str_' num2str(n_aligned_depths) '_depths_long_wfonly'];
+save([save_path filesep save_fn],'-v7.3');
+
 %% Batch load and save activity from all choiceworld (common U - LONG + ctx->str)
 
 n_aligned_depths = 4;
@@ -7580,7 +7695,7 @@ for curr_animal = 1:length(animals)
     load_parts.imaging = true;
     load_parts.ephys = false;
     
-    for curr_day = 1:length(experiments);
+    for curr_day = 1:length(experiments)
         
         day = experiments(curr_day).day;
         experiment = experiments(curr_day).experiment;
