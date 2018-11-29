a8f848c95e3d5d0ecbf2817f390951add6ac0639
Andrew Peters <peters.andrew.j@gmail.com>
2018-11-29 16:19:54 +0000

    update

--- general/AP_stackplot.m
+++ general/AP_stackplot.m
@@ -1,20 +1,26 @@
-function h = AP_stackplot(x,t,spacing,zs,color,ylabels)
-% h = AP_stackplot(x,t,spacing,zs,color,ylabels)
+function h = AP_stackplot(x,t,spacing,zs,color,ylabels,zero_lines)
+% h = AP_stackplot(x,t,spacing,zs,color,ylabels,zero_lines)
 %
 % Plot lines stacked on each other
 % x - matrix to plot, N points x M lines
 % spacing - spacing between lines
 % zscore - true/false, zscore traces
 % ylabels - can set the ylabels for each line being plotted
+% zero_lines - plot horizontal lines at zero for each line
 
+% Set defaults
 if ~exist('t','var') || isempty(t)
     t = 1:size(x,1);
 end
 if ~exist('spacing','var') || isempty(spacing)
-    spacing = 5;
+    % default spacing is a little larger than the range of the data
+    spacing = range(x(:))*1.2;
 end
 if ~exist('zs','var') || isempty(zs)
    zs = false; 
+end
+if ~exist('zero_lines','var') || isempty(zero_lines)
+    zero_lines = false;
 end
 
 % Stack from top to bottom
@@ -25,7 +31,7 @@ elseif zs
     x_spaced = bsxfun(@plus,zscore(x,[],1),spacing_add);
 end
 
-if exist('color','var') && ~isempty(color);
+if exist('color','var') && ~isempty(color)
     if length(color) == 1
         h = plot(t,x_spaced,'linewidth',2,'color',color);
     elseif size(color,1) == 1 && size(color,2) == 3
@@ -39,10 +45,22 @@ else
     h = plot(t,x_spaced,'linewidth',2);
 end
 
-% Set ylabels if desired
+% Set ylabels
 if exist('ylabels','var')
     set(gca,'YTick',sort(spacing_add));
     set(gca,'YTickLabel',flipud(ylabels(:)));
 end
+
+% Plot horizontal zero-lines 
+if zero_lines
+    for i = 1:size(x,2)
+        axis tight
+        line(xlim,repmat(spacing_add(i),1,2),'color','k');
+    end
+end
+
+
+
+
 
 
--- load_and_process/AP_load_concat_normalize_ctx_str.m
+++ load_and_process/AP_load_concat_normalize_ctx_str.m
@@ -1,42 +1,58 @@
-function AP_load_concat_normalize_ctx_str(data_fn,exclude_data)
-% data
+function [fluor_allcat,mua_allcat,wheel_allcat,reward_allcat,D_allcat] = ...
+    AP_load_concat_normalize_ctx_str(data_fn,exclude_data)
+% [fluor_allcat,mua_allcat,wheel_allcat,reward_allcat,D_allcat] = ...
+%     AP_load_concat_normalize_ctx_str(data_fn,exclude_data)
+%
+% Loads, concatenates, and normalizes trial data for the ctx-str project
+% data_fn - filename (e.g. trial_activity_choiceworld)
+% exclude_data - true/false at the moment (for bad behavior days)
+% (can in future extend this to wonky recording days?)
 
 % Load data
 data_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\paper\data';
 load([data_path filesep data_fn]);
 n_animals = length(D_all);
 
-% Get time
+% Get time (should save this in the data)
 framerate = 35;
 raster_window = [-0.5,3];
 upsample_factor = 3;
 sample_rate = (framerate*upsample_factor);
 t = raster_window(1):1/sample_rate:raster_window(2);
 
-% Load use experiments and cut out bad ones
-exclude_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\experiment_exclusion';
-exclude_fn{1} = 'bhv_use_experiments';
-% exclude_fn{2} = 'expl_var_use_experiments';
-use_experiments_all = {};
-for curr_exclude = 1:length(exclude_fn)
-    curr_use_experiments = load([exclude_path filesep exclude_fn{curr_exclude}]);
-    use_experiments_all = [use_experiments_all;curr_use_experiments.use_experiments];
+% Load pre-marked experiments to exclude and cut out bad ones
+if exist('exclude_data','var') && isempty(exclude_data)
+    if exclude_data
+        exclude_path = 'C:\Users\Andrew\OneDrive for Business\Documents\CarandiniHarrisLab\analysis\wf_ephys_choiceworld\experiment_exclusion';
+        exclude_fn{1} = 'bhv_use_experiments';
+        % exclude_fn{2} = 'expl_var_use_experiments';
+        use_experiments_all = {};
+        for curr_exclude = 1:length(exclude_fn)
+            curr_use_experiments = load([exclude_path filesep exclude_fn{curr_exclude}]);
+            use_experiments_all = [use_experiments_all;curr_use_experiments.use_experiments];
+        end
+        use_experiments = arrayfun(@(x) all(vertcat(use_experiments_all{:,x}),1), ...
+            1:size(use_experiments_all,2),'uni',false);
+        
+        D_all = cellfun(@(data,use_expts) data(use_expts),D_all,use_experiments','uni',false);
+        fluor_all = cellfun(@(data,use_expts) data(use_expts),fluor_all,use_experiments','uni',false);
+        mua_all = cellfun(@(data,use_expts) data(use_expts),mua_all,use_experiments','uni',false);
+        wheel_all = cellfun(@(data,use_expts) data(use_expts),wheel_all,use_experiments','uni',false);
+        if exist('reward_all','var')
+            reward_all = cellfun(@(data,use_expts) data(use_expts),reward_all,use_experiments','uni',false);
+        end
+    end
 end
-use_experiments = arrayfun(@(x) all(vertcat(use_experiments_all{:,x}),1), ...
-    1:size(use_experiments_all,2),'uni',false);
 
-D_all = cellfun(@(data,use_expts) data(use_expts),D_all,use_experiments','uni',false);
-fluor_all = cellfun(@(data,use_expts) data(use_expts),fluor_all,use_experiments','uni',false);
-mua_all = cellfun(@(data,use_expts) data(use_expts),mua_all,use_experiments','uni',false);
-wheel_all = cellfun(@(data,use_expts) data(use_expts),wheel_all,use_experiments','uni',false);
-reward_all = cellfun(@(data,use_expts) data(use_expts),reward_all,use_experiments','uni',false);
-
+% If any animals have empty data, remove them
 use_animals = cellfun(@(x) ~isempty(x),D_all);
 D_all = D_all(use_animals);
 fluor_all = fluor_all(use_animals);
 mua_all = mua_all(use_animals);
 wheel_all = wheel_all(use_animals);
-reward_all = reward_all(use_animals);
+if exist('reward_all','var')
+    reward_all = reward_all(use_animals);
+end
 
 % Get number of widefield components and MUA depths
 n_vs = size(fluor_all{1}{1},3);
@@ -47,12 +63,7 @@ fluor_allcat = nan(0,length(t),n_vs,1);
 mua_allcat = nan(0,length(t),n_depths,1);
 wheel_allcat = nan(0,length(t),1);
 reward_allcat = nan(0,length(t),1);
-
-trial_contrast_allcat = [];
-trial_side_allcat = [];
-trial_choice_allcat = [];
-
-D_cat = struct('stimulus',cell(1,1),'response',cell(1,1),'repeatNum',cell(1,1));
+D_allcat = struct('stimulus',cell(1,1),'response',cell(1,1),'repeatNum',cell(1,1));
 
 for curr_animal = 1:length(D_all)
     
@@ -96,34 +107,25 @@ for curr_animal = 1:length(D_all)
     
     % Concatenate wheel
     wheel_cat = cat(1,wheel_all{curr_animal}{:});
-    %     wheel_cat_norm = wheel_cat./prctile(max(max(abs(wheel_cat),[],2),[],3),95);
     
     % Concatenate behavioural data
-    D = struct;
-    D.stimulus = cell2mat(cellfun(@(x) x.stimulus,D_all{curr_animal},'uni',false));
-    D.response = cell2mat(cellfun(@(x) x.response,D_all{curr_animal},'uni',false));
-    D.repeatNum = cell2mat(cellfun(@(x) x.repeatNum,D_all{curr_animal},'uni',false));
-    
-    D.day = trial_day;
-    
-    D_cat.stimulus = vertcat(D_cat.stimulus,D.stimulus);
-    D_cat.response = vertcat(D_cat.response,D.response);
-    D_cat.repeatNum = vertcat(D_cat.repeatNum,D.repeatNum);
-    
-    % Get trial ID
-    trial_contrast = max(D.stimulus,[],2);
-    [~,side_idx] = max(D.stimulus > 0,[],2);
-    trial_side = (side_idx-1.5)*2;
-    trial_choice = -(D.response-1.5)*2;
-    
-    % Concatenate everything
+    stimulus = cell2mat(cellfun(@(x) x.stimulus,D_all{curr_animal},'uni',false));    
+    if exist('reward_all','var')
+        response = cell2mat(cellfun(@(x) x.response,D_all{curr_animal},'uni',false));
+        repeatNum = cell2mat(cellfun(@(x) x.repeatNum,D_all{curr_animal},'uni',false));  
+    end
+
+    % Concatenate to across-day data
     fluor_allcat = [fluor_allcat;fluor_cat];
     mua_allcat = [mua_allcat;mua_cat_norm];
     wheel_allcat = [wheel_allcat;wheel_cat];
-    reward_allcat = [reward_allcat;vertcat(reward_all{curr_animal}{:})];
-    
-    trial_contrast_allcat = [trial_contrast_allcat;trial_contrast];
-    trial_side_allcat = [trial_side_allcat;trial_side];
-    trial_choice_allcat = [trial_choice_allcat;trial_choice];
+    D_allcat.stimulus = vertcat(D_allcat.stimulus,stimulus);
+    if exist('reward_all','var')
+        reward_allcat = [reward_allcat;vertcat(reward_all{curr_animal}{:})];
+        D_allcat.response = vertcat(D_allcat.response,response);
+        D_allcat.repeatNum = vertcat(D_allcat.repeatNum,repeatNum);
+    end
     
 end
+
+
