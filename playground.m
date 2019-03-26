5443a31bd5c8c10ee177e73e0bde11b7573bbce0
Andrew Peters <peters.andrew.j@gmail.com>
2019-03-26 11:59:32 +0000

    update

--- playground.m
+++ playground.m
@@ -5902,7 +5902,7 @@ plot(t,nanmean(mua_allcat_move(plot_trials,:,2),1) - ...
 
 animals = {'AP024','AP025','AP026','AP027','AP028','AP029'};
 
-for curr_animal = 5:length(animals)
+for curr_animal = 1:length(animals)
     
     animal = animals{curr_animal};
     protocol = 'vanillaChoiceworld';
@@ -5911,12 +5911,7 @@ for curr_animal = 5:length(animals)
     experiments = experiments([experiments.imaging] & [experiments.ephys]); 
     
     for curr_day = 1:length(experiments)
-        
-        % (from server restart)
-        if curr_animal == 5 && curr_day < 7
-            continue
-        end
-        
+              
         day = experiments(curr_day).day;        
         disp(['Kilosorting ' animal ' ' day '(' num2str(curr_day) '/' num2str(length(experiments)) ')']);       
         AP_preprocess_phase3(animal,day);
