0aa94a7054c975585ac79adcf470cdd2439f0d11
Andrew Peters <peters.andrew.j@gmail.com>
2019-06-03 18:01:04 +0100

    update

---
+++ plotting_and_utilities/AP_heatscatter.m
@@ -0,0 +1,53 @@
+function AP_heatscatter(x,y,n_bins)
+% AP_heatscatter(x,y,n_bins)
+%
+% Scatter plot turning into heatmap when dense
+% x = x data
+% y = y data
+% n_bins = number of histogram bins for x and y
+
+% Cut out NaNs
+nan_points = isnan(x) | isnan(y);
+x(nan_points) = [];
+y(nan_points) = [];
+
+% Set heat/scatter threshold
+% (don't need to change: number of overlapping dots is constant)
+max_bin_point_plot = 20; % threshold for heatmap vs scatter
+
+% Set bins and bin data
+x_bins = linspace(min(x),max(x),n_bins+1);
+y_bins = linspace(min(y),max(y),n_bins+1);
+
+x_bin_centers = x_bins(1:end-1) + diff(x_bins)./2;
+y_bin_centers = y_bins(1:end-1) + diff(y_bins)./2;
+
+[bin_n,~,~,bin_x,bin_y] = histcounts2(x,y,x_bins,y_bins);
+bin_n = bin_n'; % histcounts2 returns a transposed x/y?! that's dumb
+bin_idx = sub2ind([size(bin_n,1),size(bin_n,2)],bin_y,bin_x);
+
+% Set heat bins to image and scatter points to plot
+heat_bins = bin_n > max_bin_point_plot;
+scatter_bins = bin_n <= max_bin_point_plot; 
+scatter_points = scatter_bins(bin_idx);
+
+% TEMP: sqrt bin_n for skewed data?
+bin_n = sqrt(bin_n);
+
+% Convert bins to 255-scale color index, get color by density bin
+density_col = hot(255);
+bin_n_col_idx = round((bin_n - min(bin_n(:)))./(max(bin_n(:)) - min(bin_n(:)))*254)+1;
+point_col_idx = bin_n_col_idx(bin_idx);
+point_col = density_col(point_col_idx,:);
+
+% Plot scatter plot colored by density
+hold on; colormap(hot)
+
+heat_points = imagesc(x_bin_centers,y_bin_centers,bin_n);
+set(heat_points,'AlphaData',heat_bins);
+
+scatter(x(scatter_points),y(scatter_points),5,point_col(scatter_points,:),'filled');
+
+
+
+
