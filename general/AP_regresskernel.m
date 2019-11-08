a21053a9e19626b04afa3e60d7eeac2756d7faf9
Andrew Peters <peters.andrew.j@gmail.com>
2019-11-08 19:00:58 +0000

    update

--- general/AP_regresskernel.m
+++ general/AP_regresskernel.m
@@ -186,7 +186,7 @@ cv_partition = nan(size(regressor_design,1),1);
 cv_partition(predictable_samples) = min(floor(linspace(1,cvfold+1,sum(predictable_samples))),cvfold)';
 
 % (to shuffle CV points instead of using consecutive chunks)
-% (I THINK THIS IS INVALID FOR TIMESERIES DATA?)
+% (I THINK THIS IS INVALID FOR TIMESERIES DATA? Why was this here?)
 % cv_partition(predictable_samples) = AP_shake(round(linspace(1,cvfold,sum(predictable_samples)))');
 
 k_cv = nan(size(regressors_gpu,2),size(signals,1),cvfold,'single');
